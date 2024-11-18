516b0da27a029cb5ff44d18990fbeb82086246f3
Ben Nguyen <benc.nguyen02@gmail.com>
2024-11-17 16:59:37 -0800

    BANK ACCOUNT COMPLETED

---
+++ src/ca/bcit/comp2522/lab10/bam/BankAccount.java
@@ -0,0 +1,70 @@
+package ca.bcit.comp2522.lab10.bam;
+/**
+ * Represents a bank account with basic operations to deposit, withdraw,
+ * and retrieve the current balance.
+ *
+ * @author Ben Nguyen, Marcus Lages, Andre Dizon
+ * @version 1.0
+ */
+public class BankAccount {
+    private int currentBalance;
+    private static final int NO_FUNDS = 0;
+
+    /**
+     * Constructs a BankAccount object with the specified initial balance.
+     *
+     * @param currentBalance the initial balance of the account in USD.
+     * @throws IllegalArgumentException if the initial balance is negative.
+     */
+    public BankAccount(final int currentBalance) {
+        validateCurrentBalance(currentBalance);
+        this.currentBalance = currentBalance;
+    }
+
+    /**
+     * Retrieves the current balance of the account in USD.
+     *
+     * @return the current balance.
+     */
+    public int getBalanceUSD() {
+        return currentBalance;
+    }
+
+    /**
+     * Deposits the specified amount into the account.
+     *
+     * @param amount the amount to deposit, must be a positive value.
+     * @throws IllegalArgumentException if the deposit amount is negative.
+     */
+    public void deposit(final int amount) {
+        if (amount < NO_FUNDS) {
+            throw new IllegalArgumentException("Deposit amount must be non-negative.");
+        }
+        currentBalance += amount;
+    }
+
+    /**
+     * Withdraws the specified amount from the account.
+     *
+     * @param amount the amount to withdraw, must not exceed the current balance.
+     * @throws IllegalArgumentException if the withdrawal amount exceeds the current balance.
+     */
+    public void withdraw(final int amount) {
+        if (amount > currentBalance) {
+            throw new IllegalArgumentException("Insufficient funds.");
+        }
+        currentBalance -= amount;
+    }
+
+    /**
+     * Validates the specified initial balance to ensure it is non-negative.
+     *
+     * @param currentBalance the balance to validate.
+     * @throws IllegalArgumentException if the balance is negative.
+     */
+    public void validateCurrentBalance(final int currentBalance) {
+        if (currentBalance < NO_FUNDS) {
+            throw new IllegalArgumentException("Initial balance cannot be negative.");
+        }
+    }
+}
--- src/ca/bcit/comp2522/lab10/bam/Main.java
+++ src/ca/bcit/comp2522/lab10/bam/Main.java
@@ -1,3 +1,4 @@
+package ca.bcit.comp2522.lab10.bam;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
